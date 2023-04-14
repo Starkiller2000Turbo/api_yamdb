681ed108a83f55ab321a931549068da3b0f971ac
Максим Ермошин <maksim_ermoshin@mail.ru>
2023-04-14 17:14:37 +0300

    Removed id

--- api_yamdb/compositions/serializers.py
+++ api_yamdb/compositions/serializers.py
@@ -3,14 +3,26 @@ from rest_framework import serializers
 from compositions.models import Category, Genre, Title
 
 
+class GenreSerializer(serializers.ModelSerializer):
+    class Meta:
+        fields = (
+            'name',
+            'slug',
+        )
+        model = Genre
+
+
+class CategorySerializer(serializers.ModelSerializer):
+    class Meta:
+        fields = (
+            'name',
+            'slug',
+        )
+        model = Category
+
+
 class TitleSerializer(serializers.ModelSerializer):
-    genre = serializers.ListField(
-        child=serializers.SlugRelatedField(
-            slug_field='slug',
-            queryset=Genre.objects.all(),
-        ),
-        allow_empty=False,
-    )
+    genre = GenreSerializer(many=True)
     category = serializers.SlugRelatedField(
         slug_field='slug',
         queryset=Category.objects.all(),
@@ -26,23 +38,3 @@ class TitleSerializer(serializers.ModelSerializer):
             'category',
         )
         model = Title
-
-
-class GenreSerializer(serializers.ModelSerializer):
-    class Meta:
-        fields = (
-            'id',
-            'name',
-            'slug',
-        )
-        model = Genre
-
-
-class CategorySerializer(serializers.ModelSerializer):
-    class Meta:
-        fields = (
-            'id',
-            'name',
-            'slug',
-        )
-        model = Category
